9cac43cf601088a89b9eb51e44209f8f77e004b8
MDey <Maitreyee Dey>
2026-05-29 13:26:14 +0530

    update on gateway Prod program.cs

--- AppointmentScheduler.Gateway/Program.cs
+++ AppointmentScheduler.Gateway/Program.cs
@@ -10,6 +10,20 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddHttpClient("AppointmentService", client =>
+{
+    client.BaseAddress = new Uri(
+        builder.Configuration["Services:AppointmentService"]!);
+});
+
+builder.Services.AddHttpClient("BookingService", client =>
+{
+    client.BaseAddress = new Uri(
+        builder.Configuration["Services:BookingService"]!);
+});
+
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(builder =>
@@ -27,17 +41,28 @@ builder.Services.AddSwaggerGen(options =>
     options.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
 });
 
+/*
+
 builder.Services.AddHttpClient("AppointmentService", client =>
 {
     client.BaseAddress =
         new Uri("http://localhost:5089/");
+        //for Production
+    client.BaseAddress =
+        new Uri("https://appointmentservice.yellowpebble-e1ad0743.centralindia.azurecontainerapps.io/");
 });
+
 
 builder.Services.AddHttpClient("BookingService", client =>
 {
     client.BaseAddress =
         new Uri("http://localhost:5097/");
+        //for Production
+    client.BaseAddress =
+        new Uri("https://bookingservice.yellowpebble-e1ad0743.centralindia.azurecontainerapps.io/");
 });
+*/
+
 var jwtSettings = builder.Configuration.GetSection("JwtSettings");
 var key = jwtSettings["Key"] ?? throw new InvalidOperationException("JWT key is required.");
 var issuer = jwtSettings["Issuer"] ?? "AppointmentScheduler";
